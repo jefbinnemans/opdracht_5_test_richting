6b19792cb17b4bfd84dc1ff9162e8437d7fab160
jefbinnemans <41265139+jefbinnemans@users.noreply.github.com>
2020-11-08 11:44:16 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 /**
- * Draai met het microbit logo in de simulator om de richting te veranderen
+ * Druk op knop A in de simulator.
+ * 
+ * Dan kan je met het microbit logo draaien in de simulator om de richting te veranderen
  */
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.compassHeading())
